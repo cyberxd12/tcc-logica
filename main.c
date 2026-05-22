b4df5255b87d57e1e26570d3e794a8fffb435208
cyberxd12 <luizfelipe122724@gmail.com>
2026-05-22 09:16:04 -0300

    start modulo 1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char nome[50];
+    int dano;
+    float preço;
+} equipamento;
+
+int main(void)
+{
+    equipamento catalogo[10];
+
+    return 0;
+}
