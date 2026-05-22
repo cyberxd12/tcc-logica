4a263324e2d205a0d80ca13396ce39939d7e38ec
cyberxd12 <luizfelipe122724@gmail.com>
2026-05-22 17:15:25 -0300

    modulo 1: criação da fila com inserçoes de teste

--- main.c
+++ main.c
@@ -2,15 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+//estrutura do equipamento
+typedef struct equipamento {
     char nome[50];
     int dano;
-    float preço;
+    float preco;
+    struct equipamento *prox;
 } equipamento;
+
+//estrutura da fila
+typedef struct fila{
+    equipamento *inicio;
+    equipamento *fim;
+}fila;
+
+void inserir_equipamento(fila *f, char *nome, int dano, float preco){
+    //criando novo equipamento
+    equipamento *novo = (equipamento*) malloc(sizeof(equipamento));
+    
+    //atribuindo valores
+    strcpy(novo->nome, nome);
+    novo->dano = dano;
+    novo->preco = preco;
+    novo->prox = NULL;
+    
+    //debug
+    printf("\ncadastrando produto");
+    printf("\nnome: %s", novo->nome);
+    printf("\ndano: %i", novo->dano);
+    printf("\npreco: %.2f", novo->preco);
+    
+    //teste sequencia da fila
+    if(f->inicio == NULL){
+        //se estiver vazia ele assume as duas posiçoes
+        f->inicio = novo;
+        f->fim = novo;
+    }else{
+        //se já tem algo na fila o proximo do atual sera o novo e o novo sera o ultimo
+        f->fim->prox = novo;
+        f->fim= novo;
+    }
+    
+    printf("\nconcluido com sucesso!\n");
+}
 
 int main(void)
 {
-    equipamento catalogo[10];
+    //vetor com 10 amazenamentos para equipamentos
+    fila catalogo;
 
+    //inserindo equipamentos
+    inserir_equipamento(&catalogo, "espada", 10, 11.5);
+    inserir_equipamento(&catalogo, "escudo", 2, 9.0);
+    inserir_equipamento(&catalogo, "capacete", 0, 3.0);
+
+   
     return 0;
 }
