0af2d6acd4e724eb9ac9a277f7bfed1d22935e75
luiz felipe <luizfelipe122724@gmail.com>
2026-05-25 13:22:11 -0300

    modulo 1: menus e catalogo padrão

--- main.c
+++ main.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "modulo1"
+
+int opc;
+
+void menu_principal(){  
+    do{
+        printf("\n\nQual o modulo que deseja acessar:\n1-arsenal da guilda\n2-fila da enfermaria\n3-Mural de Missões\n4- Fechamento da Taverna\n0-sair do sistema\nescolha: ");
+        scanf(" %d", &opc);
+
+        switch(opc){
+            case 1:
+                printf("\nAbrindo arsenal da guilda...\n");
+                arsenal();
+            break;
+
+            case 2:
+
+            break;
+
+            case 0:
+                opc == 0;
+            break;
+
+            default:
+                printf("\nopção invalida...\n");
+            break;
+        }
+    }while(opc !=0);
+}
 
 int main(void)
 {
-    //vetor com 10 amazenamentos para equipamentos
-    fila catalogo;
-
-    //inserindo equipamentos
-    inserir_equipamento(&catalogo, "espada", 10, 11.5);
-    inserir_equipamento(&catalogo, "escudo", 2, 9.0);
-    inserir_equipamento(&catalogo, "capacete", 0, 3.0);
+    menu_principal();
 
     return 0;
 }
